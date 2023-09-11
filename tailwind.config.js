709efb8461bf4d8e0535b446c22cb6145e9824a0
MateenTech <mbhutto.official@gmail.com>
2023-09-11 16:07:12 +0500

    website build and readme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-grayish-magenta': 'hsl(300, 24%, 96%)',
+        'very-dark-magenta': 'hsl(300, 43%, 22%)',
+        'pink-grayish': 'hsl(303, 10%, 53%)',
+        'soft-pink':'hsl(333, 80%, 67%)',
+      }
+    },
   },
   plugins: [],
 }
